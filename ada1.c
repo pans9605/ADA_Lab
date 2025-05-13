48339e0b4c2ea1a7cd248cc9fee25cc8e704a530
pans9605 <parnika0905@gmail.com>
2025-05-13 21:19:38 +0530

    Update ada1.c

--- ada1.c
+++ ada1.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
-
 #define MAX 100
 
-int adj[MAX][MAX], n, visited[MAX], stack[MAX], top = -1;
-
-void dfs(int v) {
+void dfs(int v, int n, int adj[MAX][MAX], int visited[MAX], int stack[MAX], int *top) {
     visited[v] = 1;
     for (int i = 0; i < n; i++)
         if (adj[v][i] && !visited[i])
-            dfs(i);
-    stack[++top] = v;
+            dfs(i, n, adj, visited, stack, top);
+    stack[++(*top)] = v;
 }
 
-void topologicalSort() {
+void topologicalSort(int n, int adj[MAX][MAX]) {
+    int visited[MAX] = {0}, stack[MAX], top = -1;
     for (int i = 0; i < n; i++)
         if (!visited[i])
-            dfs(i);
+            dfs(i, n, adj, visited, stack, &top);
     while (top >= 0)
         printf("%d ", stack[top--]);
 }
 
 int main() {
-    int edges, u, v;
+    int n, edges, u, v, adj[MAX][MAX] = {0};
     printf("Enter number of vertices and edges: ");
     scanf("%d %d", &n, &edges);
-
     for (int i = 0; i < edges; i++) {
         scanf("%d %d", &u, &v);
         adj[u][v] = 1;
     }
-
     printf("Topological Order: ");
-    topologicalSort();
+    topologicalSort(n, adj);
     return 0;
 }
